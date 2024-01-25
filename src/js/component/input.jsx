e1e39b5ba94de0e8feaef4d9ebfa546f69e3679d
Karen Lyon <144794853+Karelyon@users.noreply.github.com>
2024-01-25 22:07:05 +0000

    changes

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,70 +1,69 @@
 import React, { useEffect, useState } from "react";
 
 const Input = () => {
-   
 
-    // const validateInput = () => {
-    //     if (inputValue === "") alert("The input cannot be empty");
-    //     else (setItem(item.concat(inputValue)));
-    // }
-    // const validate = (value) => {
-    //     if (value === ' ') { alert("The input its not correct") } else (setInputValue(value))
-    // }
 
-    // const validateTask = (item) => {
-    //     if (item == 0) { return <p>No items yet</p> }
-    //     else if (item === 1) { return <p>{item} item left</p> }
-    //     else return <p>{item} items left</p>;
-    // }
+    const validateInput = () => {
+        if (inputValue === "") alert("The input cannot be empty");
+        else (setItem(item.concat(inputValue)));
+    }
+    const validate = (value) => {
+        if (value === ' ') { alert("The input its not correct") } else (setInputValue(value))
+    }
     
+    const validateTask = (item) => {
+        if (item == 0) { return <p>No items yet</p> }
+        else if (item === 1) { return <p>{item} item left</p> }
+        else return <p>{item} items left</p>;
+    }
+
     const [inputValue, setInputValue] = useState("");
     const [item, setItem] = useState([]);
 
-
-
-
+   
     // Para obtener las tareas
     const getTask = () => {
+       
         fetch('https://playground.4geeks.com/apis/fake/todos/user/Karelyon', {
             method: 'GET',
             headers: {
                 'Content-type': 'application/json'
             }
-        }).then(response => response.json()) 
+        }).then(response => response.json())
             .then(
-                response => setItem(response)
-            );
+ response => setItem(response))
+         
     }
-
+  
     // cambia el estado
     useEffect(() => {
         getTask();
     }, []);
 
-    
+
     // Para actualizar la tarea 
-        const UpdateTask = (value) => {
-           const newTask = {
+    const UpdateTask = (value) => {
+        const newTask = {
             label: value,
             done: false
-           }
-           const updateList = [...item,newTask]
-            const requestOptions = { 
-                method : 'PUT',
-                headers :{
-                     'Content-type' : 'application/json'
-                },
-                body : JSON.stringify(updateList)
-            }
-            fetch('https://playground.4geeks.com/apis/fake/todos/user/Karelyon',requestOptions)
-            .then((response)=>{
+        }
+        const updateList = [...item, newTask]
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updateList)
+        }
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/Karelyon', requestOptions)
+            .then((response) => {
                 if (response.ok) {
                     setInputValue("");
                     return getTask()
-                }else {
-                    throw new Error ("Error al agregar la tarea")
+                } else {
+                    throw new Error("Error al agregar la tarea")
                 }
-            }).catch ((error)=>{console.error("Error al agregar la tarea",error)})
+            }).catch((error) => { console.error("Error al agregar la tarea", error) })
 
     }
 
@@ -73,42 +72,50 @@ const Input = () => {
     const deleteItem = (deletedItem) => {
         const newList = item.filter((listitem) => listitem.id !== deletedItem.id);
         setItem(newList);
-        return  fetch ('https://playground.4geeks.com/apis/fake/todos/user/Karelyon', {
+        return fetch('https://playground.4geeks.com/apis/fake/todos/user/Karelyon', {
             method: 'PUT',
             headers: {
                 'Content-type': 'application/json'
             },
             body: JSON.stringify(newList)
-    }) .then((response)=>{
-        if (response.ok) {
-            setInputValue("");
-            return getTask()
-        }else {
-            throw new Error ("Error al agregar la tarea")
-        }
-    }).catch ((error)=>{console.error("Error al agregar la tarea",error)}) ;}
-    
-    // Primero, hacemos la solicitud DELETE para borrar el elemento
+        }).then((response) => {
+            if (response.ok) {
+                setInputValue("");
+                return getTask()
+            } else {
+                throw new Error("Error al agregar la tarea")
+            }
+        }).catch((error) => { console.error("Error al agregar la tarea", error) });
+    }
 
+
+    const DeleteAll = () => {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/Karelyon', {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        }).then((response) =>{ if(response.ok){setItem([])}})
+    }
     return (
         <div className="back">
 
             <h1 className="text-center">To Do List</h1>
             <ul>
                 <li >
-                    <input type="text" value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}} placeholder="Write your new task"
-                        onKeyDown={(e)=>{
-                            if(e.key==="Enter"){
-                                UpdateTask(inputValue)
+                    <input type="text" value={(inputValue)} onChange={(e) => {validate(e.target.value)}}  placeholder="Write your new task"
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                {validateInput()(UpdateTask(inputValue))}
                             }
                         }}
                     />
 
                 </li>
 
-                {item.map(listitem=> {
-                    return(
-                
+                {item.map(listitem => {
+                    return (
+
                         <li key={listitem.id} className="listInput d-flex justify-content-between align-items-center ">
                             {listitem.label}
 
@@ -117,9 +124,11 @@ const Input = () => {
                 })}
             </ul>
             <div className="task">
-                <p>{item.length}</p>
+                <p>{validateTask(item.length)}</p>
             </div>
-
+            <div>
+            <button type="button" className="btn btn-danger" onClick={()=>DeleteAll(alert("Don't Forget to create a new User"))}>DELETE EVERYTHING</button>
+            </div>
         </div>
 
 
